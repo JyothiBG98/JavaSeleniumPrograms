7a177d38d74efe49ed3188b9663d6eb15e53d61d
JyothiBG98 <jyothibg1217@gmail.com>
2023-01-30 21:31:54 -0800

    Adding condition statement files.

--- src/test/java/programs/forLoop.java
+++ src/test/java/programs/forLoop.java
@@ -2,10 +2,18 @@ package programs;
 
 public class forLoop {
     public static void main(String[] args) {
+        /*
+        Syntax for forloop
+        for(initialisation; condition statement; increment/decrement )
+         */
         for(int i=0;i<=10;i++)
         {
             System.out.println(i);
         }
+
+        /*
+        To check whether the given name is found or not.
+         */
         String names[]={"Aravind","Jyothi","Janu","Druthi","Shresta"};
         for(String ref:names){
             if(ref=="Manu") {
